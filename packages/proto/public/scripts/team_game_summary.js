5f6ced5ca4d486a6d72037c55fa42e56fa86588e
Kevin Tan <k.tann.578@gmail.com>
2024-10-22 13:19:23 -0700

    Update team_game_summary.js

--- packages/proto/public/scripts/team_game_summary.js
+++ packages/proto/public/scripts/team_game_summary.js
@@ -62,38 +62,8 @@ export class TeamGameSummaryElement extends HTMLElement {
                 </tr>
             </thead>
             <tbody>
-                <tr slot="player">
-                    <td slot="player_name_champ">
-                            <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/tiles/Gnar_0.jpg" class="champ_icon">
-                            <a href="">Azhi</a>
-                        </td>
-                        <td slot="runes_summoner_spells">
-                            <img src="https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/Resolve/GraspOfTheUndying/GraspOfTheUndying.png"
-                                class="rune">
-                            <img src="https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/7201_Precision.png"
-                                class="rune">
-                            <img src="https://ddragon.leagueoflegends.com/cdn/14.20.1/img/spell/SummonerFlash.png"
-                                class="summoner_spell">
-                            <img src="https://ddragon.leagueoflegends.com/cdn/14.20.1/img/spell/SummonerTeleport.png"
-                                class="summoner_spell">
-                        </td>
-                        <td slot="items">
-                            <img src="https://ddragon.leagueoflegends.com/cdn/14.20.1/img/item/1057.png" class="item">
-                            <img src="https://ddragon.leagueoflegends.com/cdn/14.20.1/img/item/1031.png" class="item">
-                            <img src="https://ddragon.leagueoflegends.com/cdn/14.20.1/img/item/1055.png" class="item">
-                            <img src="https://ddragon.leagueoflegends.com/cdn/14.20.1/img/item/3078.png" class="item">
-                            <img src="https://ddragon.leagueoflegends.com/cdn/14.20.1/img/item/3053.png" class="item">
-                            <img src="https://ddragon.leagueoflegends.com/cdn/14.20.1/img/item/3047.png" class="item">
-                        </td>
-                        <td slot="KDA">
-                            3/0/12
-                        </td>
-                        <td slot="CS">
-                            227
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            </tbody>
+        </table>
     </template>
     `;
 
