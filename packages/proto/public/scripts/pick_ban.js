08f692f7541bb847c3f9c2170d9c98c7052b8192
Kevin Tan <k.tann.578@gmail.com>
2024-11-04 19:41:08 -0800

    formatting changes
    I honestly dont remember what these changes are. I think they are likely to me trying to make tabs for games on psg vs 100

--- packages/proto/public/scripts/pick_ban.js
+++ packages/proto/public/scripts/pick_ban.js
@@ -5,12 +5,50 @@ import header from "./styles/header.css.js";
 export class PickBanElement extends HTMLElement {
     static template = html`
     <template>
-        <div>
-            <div class="blue">
-                <slot name="ban1">
-                </slot>
+        <div class="pick_ban">
+            <div class="ban1">
+                <div class="blue">
+                    <slot name="bban1"></slot>
+                    <slot name="bban2"></slot>
+                    <slot name="bban3"></slot>
+                </div>
+                <div class="red">
+                    <slot name="rban1"></slot>
+                    <slot name="rban2"></slot>
+                    <slot name="rban3"></slot>
+                </div>
             </div>
-            <div class="red">
+            <div class="pick1">
+                <div class="blue">
+                    <slot name="bpick1"></slot>
+                    <slot name="bpick2"></slot>
+                    <slot name="bpick3"></slot>
+                </div>
+                <div class="red">
+                    <slot name="rpick1"></slot>
+                    <slot name="rpick2"></slot>
+                    <slot name="rpick3"></slot>
+                </div>
+            </div>
+            <div class="ban2">
+                <div class="blue">
+                    <slot name="bban4"></slot>
+                    <slot name="bban5"></slot>
+                </div>
+                <div class="red">
+                    <slot name="rban4"></slot>
+                    <slot name="rban5"></slot>
+                </div>
+            </div>
+            <div class="pick2">
+                <div class="blue">
+                    <slot name="bpick4"></slot>
+                    <slot name="bpick5"></slot>
+                </div>
+                <div class="red">
+                    <slot name="rpick4"></slot>
+                    <slot name="rpick5"></slot>
+                </div>
             </div>
         </div>
     </template>
@@ -20,15 +58,36 @@ export class PickBanElement extends HTMLElement {
     :host {
         display: grid;
         grid-column: 1 / -1;
+        padding-left: var(--size-spacing-xlarge);
+        padding-right: var(--size-spacing-xlarge);
     }
 
     div {
-        background-color: var(--color-background-stats)
+        align-items: center; 
+        justify-items: center;
+        // padding: var(--size-spacing-medium); 
+    }
+
+    .pick_ban {
+        display: grid;
+        grid-template-columns: 3fr 3fr 3fr 3fr;
     }
     
     .blue {
         display: grid;
-        // grid-template-columns: 1fr 1fr 1fr 0.25fr 1fr 0.25fr 1fr 1fr 0.25fr 1fr 1fr 0.25fr 1fr 1fr;
+        grid-template-columns: 1fr 1fr 1fr;
+        background: var(--color-blueside-background)
+    }
+        
+    .red {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        background: var(--color-redside-background)
+    }
+
+    .ban_phase_1, .pick_phase_1 {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
     }
     `;
 
